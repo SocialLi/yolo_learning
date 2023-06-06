0e9f60e2a447bc6bb27527b19d8d78b41b228f11
zeyi <960114038@qq.com>
2023-06-06 23:10:29 +0800

    训练脚本

--- train.sh
+++ train.sh
@@ -1,1 +1,3 @@
-python train.py --cuda -d voc -bs 8 -accu 8 -ms
+python train.py --cuda -d voc -bs 8 -accu 8 -ms \
+  --max_epoch 150 --eval_epoch 10 --wp_epoch 1 --lr_epoch 90 120 -lr 1e-3 \
+  --momentum 0.9 --weight_decay 5e-4 --gamma 0.1
